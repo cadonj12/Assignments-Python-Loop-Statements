4b50887b074642d3ee618742577c34eec58ff885
cadonj12 <cadonjohnson21@gmail.com>
2024-05-01 19:12:41 -0700

    first question

--- The Range Riddle.py
+++ The Range Riddle.py
@@ -1,2 +1,11 @@
 #Task 1
 
+import random
+
+different_moods = ["Happy", "Sad", "Energetic", "Calm"]
+different_days = ["Monday", "Tuesday", "Wednesday", "Thursday",
+"Friday", "Saturday", "Sunday"]
+
+for i in range(len(different_days)):
+    mood = random.choice(different_moods)
+    print(f"On {different_days[i]}, I was feeling {mood}.")
